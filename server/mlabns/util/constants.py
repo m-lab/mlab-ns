5201d529db94e45551f2a7fcb18baf8a58e86e5b
Claudiu Perta <claudiu.perta@gmail.com>
2012-09-25 09:25:28 +0200

    Removed unused flags.py file. Changed UPDATE_INTERVAL value to the production value.

--- server/mlabns/util/constants.py
+++ server/mlabns/util/constants.py
@@ -4,11 +4,7 @@
 # This value should be always a couple of minutes greater than the interval
 # used to check for updates from nagios. See the configuration of the
 # '/cron/check_status' job in cron.yaml.
-
-# UPDATE_INTERVAL = 15 * 60
-
-# This value should be used only for testing.
-UPDATE_INTERVAL = 3600 * 24 * 60
+UPDATE_INTERVAL = 15 * 60
 
 # Earth radius in km.
 EARTH_RADIUS    = 6371
