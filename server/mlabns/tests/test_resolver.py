4bfd74494c6dda1ca17019dd8c13e80db20c436f
Dominic Hamon <dominic@measurementlab.net>
2012-10-09 11:05:06 -0700

    Rebasing and completing test

--- server/mlabns/tests/test_resolver.py
+++ server/mlabns/tests/test_resolver.py
@@ -8,8 +8,16 @@ class ResolverTestCase(unittest2.TestCase):
 
   def testDefaultConstructor(self):
     query = resolver.LookupQuery();
-    self.assertEqual(message.POLICY_GEO, query.policy);
-    self.assertEqual(gflags.FLAGS.ipv4, query.address_family);
+    self.assertEqual(None, query.tool_id)
+    self.assertEqual(message.POLICY_GEO, query.policy)
+    self.assertEqual(None, query.metro)
+    self.assertEqual(None, query.ip_address)
+    self.assertEqual(message.ADDRESS_FAMILY_IPv4, query.address_family)
+    self.assertEqual(None, query.city)
+    self.assertEqual(None, query.country)
+    self.assertEqual(None, query.latitude)
+    self.assertEqual(None, query.longitude)
+    self.assertEqual(None, query.response_format)
 
   def testInitializeFromDictionary(self):
     # TODO
