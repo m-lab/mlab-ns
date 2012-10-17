abc6f8899c03f76c7225cb92e39b9c6afeb371a8
Tiziana Refice <tiziana@measurementlab.net>
2012-10-17 11:59:57 -0700

    Replaced hard codded values with constants already defined in message.py

--- server/mlabns/util/util.py
+++ server/mlabns/util/util.py
@@ -1,10 +1,11 @@
-from google.appengine.ext.webapp import template
-
 import json
 
-def send_not_found(request, output_type='html'):
+from google.appengine.ext.webapp import template
+from mlabns.util import message
+
+def send_not_found(request, output_type=message.FORMAT_HTML):
     request.error(404)
-    if output_type == 'json':
+    if output_type == message.FORMAT_JSON:
         data = {}
         data['status_code'] = '404 Not found'
         json_data = json.dumps(data)
@@ -14,9 +15,9 @@ def send_not_found(request, output_type='html'):
         request.response.out.write(
             template.render('mlabns/templates/not_found.html', {}))
 
-def send_server_error(request, output_type='html'):
+def send_server_error(request, output_type=message.FORMAT_HTML):
     request.error(500)
-    if output_type == 'json':
+    if output_type == message.FORMAT_JSON:
         data = {}
         data['status_code'] = '500 Internal Server Error'
         json_data = json.dumps(data)
@@ -26,8 +27,8 @@ def send_server_error(request, output_type='html'):
         request.response.out.write(
             template.render('mlabns/templates/not_found.html', {}))
 
-def send_success(request, output_type='json'):
-    if output_type == 'json':
+def send_success(request, output_type=message.FORMAT_JSON):
+    if output_type == message.FORMAT_JSON:
         data = {}
         data['status_code'] = '200 OK'
         json_data = json.dumps(data)
