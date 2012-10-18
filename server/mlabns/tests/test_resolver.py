8f45a3c0b5e619009984946606ea6cffec49d79d
Dominic Hamon <dominic@measurementlab.net>
2012-10-18 11:19:27 -0700

    Update resolver tests to match new resolver

--- server/mlabns/tests/test_resolver.py
+++ server/mlabns/tests/test_resolver.py
@@ -1,6 +1,7 @@
 import gflags
 import unittest2
 
+from mlabns.util import constants
 from mlabns.util import message
 from mlabns.util import resolver
 
@@ -9,14 +10,15 @@ class ResolverTestCase(unittest2.TestCase):
   def testDefaultConstructor(self):
     query = resolver.LookupQuery();
     self.assertEqual(None, query.tool_id)
-    self.assertEqual(message.POLICY_GEO, query.policy)
+    self.assertEqual(None, query.policy)
     self.assertEqual(None, query.metro)
     self.assertEqual(None, query.ip_address)
-    self.assertEqual(message.ADDRESS_FAMILY_IPv4, query.address_family)
+    self.assertEqual(None, query.address_family)
     self.assertEqual(None, query.city)
     self.assertEqual(None, query.country)
     self.assertEqual(None, query.latitude)
     self.assertEqual(None, query.longitude)
+    self.assertEqual(constants.GEOLOCATION_APP_ENGINE, query.geolocation_type)
     self.assertEqual(None, query.response_format)
 
   def testInitializeFromDictionary(self):
