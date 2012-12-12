a61caed205f4d2b6f02c3808959e6d5fe1ef6a2a
Claudiu Perta <claudiu.perta@gmail.com>
2012-12-12 15:05:27 +0100

    Redirect /privacy to the mlab-ns design doc.

--- server/main.py
+++ server/main.py
@@ -2,19 +2,18 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 from mlabns.handlers import admin
-from mlabns.handlers import lookup
-from mlabns.handlers import pings
-from mlabns.handlers import registration
 from mlabns.handlers import update
+from mlabns.handlers import lookup
+from mlabns.handlers import privacy
+from mlabns.handlers import registration
 #from mlabns.handlers import log2bq
 
 app = webapp.WSGIApplication(
     [(r'/', admin.AdminHandler),
     (r'/admin.*', admin.AdminHandler),
-    (r'/pings', pings.PingsHandler),
-    (r'/register', registration.RegistrationHandler),
     (r'/cron/check_status', update.NagiosUpdateHandler),
-    (r'/cron/cleanup_pings', pings.CleanupHandler),
+    (r'/privacy', privacy.PrivacyHandler),
+    (r'/register', registration.RegistrationHandler),
 #    (r'/cron/process_logs', log2bq.Log2BigQueryHandler),
     (r'/.*', lookup.LookupHandler)],
     debug=True )
