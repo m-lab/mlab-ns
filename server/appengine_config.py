d820d02a14613722761013762e8f024e696fc655
Michael Lynch <mtlynch@google.com>
2015-04-21 13:20:03 -0400

    Fixing namespace issues in test_resolver

--- server/appengine_config.py
+++ server/appengine_config.py
@@ -1,8 +1,11 @@
 import os
 from google.appengine.api import namespace_manager
 
-major_ver, minor_ver = os.environ['CURRENT_VERSION_ID'].rsplit('.', 1)
-namespace_manager.set_namespace(major_ver)
+
+def namespace_manager_default_namespace_for_request():
+    major_ver = os.environ['CURRENT_VERSION_ID'].rsplit('.', 1)[0]
+    namespace_manager.set_namespace(major_ver)
+
 
 def webapp_add_wsgi_middleware(app):
     from google.appengine.ext.appstats import recording
