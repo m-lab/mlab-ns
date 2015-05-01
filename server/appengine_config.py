2bbe2050aa4b31deb54b20176ff467d61a9618d6
Michael Lynch <mtlynch@google.com>
2015-05-01 11:46:07 -0400

    Removing dynamic namespaces
    The dynamic namespaces seem to keep creating issues. Namespaces were introduced in 5dfbef but without any explanation and the code seems to be unrelated to the main purpose of that change. That CL modified the namespace but didn't do so in a way consistent with current documentation for appengine_config.py. I updated appengine_config.py to comply with GAE docs, but now we're seeing failures after a few hours of running, I believe because ns is querying the wrong namespace.
    
    We currently only use the default namespace for the datastore and
    memcache. I suspect that the namespace code was introduced by mistake
    originally and has never had any effect until now. Therefore I'm
    removing it so that everything resumes using the default namespace at
    all times.

--- server/appengine_config.py
+++ server/appengine_config.py
@@ -1,12 +1,3 @@
-import os
-from google.appengine.api import namespace_manager
-
-
-def namespace_manager_default_namespace_for_request():
-    major_ver = os.environ['CURRENT_VERSION_ID'].rsplit('.', 1)[0]
-    namespace_manager.set_namespace(major_ver)
-
-
 def webapp_add_wsgi_middleware(app):
     from google.appengine.ext.appstats import recording
     app = recording.appstats_wsgi_middleware(app)
