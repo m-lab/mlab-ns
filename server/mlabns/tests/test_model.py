5098ff77ff85182e1b88059d52f9b4048f611b3e
fernandalavalle <lavalle@opentechinstitute.org>
2016-04-04 14:12:25 -0400

    Rebasing from development

--- server/mlabns/tests/test_model.py
+++ server/mlabns/tests/test_model.py
@@ -1,21 +1,8 @@
 import unittest2
 
+from google.appengine.ext import testbed
+
 from mlabns.db import model
-
-
-def assertObjectSetsEqualByAttribute(test_case, set_a, set_b, attribute_lambda):
-    """Equality of sets is based on length and attribute value of objects
-
-    attribute_lambda of the form: 'lambda x: x.attribute'
-    """
-    list_a = list(set_a)
-    list_b = list(set_b)
-
-    ids_a = [attribute_lambda(x) for x in list_a]
-    ids_b = [attribute_lambda(x) for x in list_b]
-    test_case.assertEqual(len(list_a), len(list_b))
-    test_case.assertListEqual(sorted(ids_a), sorted(ids_b))
-
 
 class ModelTestCase(unittest2.TestCase):
 
@@ -35,6 +22,32 @@ class ModelTestCase(unittest2.TestCase):
         self.assertEqual(
             model.get_sliver_tool_id(tool_id, slice_id, server_id, site_id),
             'tool_id-slice_id-server_id-site_id')
+
+class GetAllToolIdsTest(unittest2.TestCase):
+
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def test_get_all_tool_ids_returns_successfully_from_datastore(self):
+        tool_a = model.Tool(tool_id='tool_a')
+        tool_b = model.Tool(tool_id='tool_b')
+        tool_c = model.Tool(tool_id='tool_c')
+        expected_ids = ['tool_a', 'tool_b', 'tool_c']
+
+        tool_a.put()
+        tool_b.put()
+        tool_c.put()
+
+        actual_ids = model.get_all_tool_ids()
+        self.assertItemsEqual(actual_ids, expected_ids)
+
+    def test_get_all_tool_ids_no_stored_tools_returns_empty(self):
+        self.assertItemsEqual(model.get_all_tool_ids(), [])
 
 
 if __name__ == '__main__':
