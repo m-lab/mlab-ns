e14a6577bc1be4ee11752f435fb688dcc44f5714
yachang <yachang@users.noreply.github.com>
2018-02-15 10:21:33 -0500

    Update sliver_tool_fetcher.py

--- server/mlabns/db/sliver_tool_fetcher.py
+++ server/mlabns/db/sliver_tool_fetcher.py
@@ -90,9 +90,12 @@ def _filter_choose_one_host_per_site(tools):
                 if tool.site_id == RoundRobinSites[i]:
                     # we need to decide whether
                     is_round_robin = True
-                    if random.uniform(0, 1) > 1.0 / float(RRCount[i]):
+                    logging.info('Before RR %s', sites[tool.site_id].server_id)
+                    logging.info('new one: %s', tool.server_id)
+                    if random.uniform(0, 1) < 1.0 / float(RRCount[i]):
                         sites[tool.site_id] = tool
                     RRCount[i] = RRCount[i] + 1
+                    logging.info('After %s', sites[tool.site_id].server_id)
                     break
             # instead of always return mlab1, we pick one randomly.
             logging.info('Before %s', sites[tool.site_id].server_id)
