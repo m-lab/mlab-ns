07e93c0b02fb4ef7a7e3c800b424152a7ab125ec
yachang <yachang@users.noreply.github.com>
2018-02-26 15:09:22 -0500

    Update sliver_tool_fetcher.py

--- server/mlabns/db/sliver_tool_fetcher.py
+++ server/mlabns/db/sliver_tool_fetcher.py
@@ -89,11 +89,11 @@ def _filter_choose_one_host_per_site(tools):
                     RoundRobinCounter[tool.site_id] = 3
                 else:
                     if random.uniform(
-                        0,
-                        1) < 1.0 / float(RoundRobinCounter[tool.site_id]):
+                            0,
+                            1) < 1.0 / float(RoundRobinCounter[tool.site_id]):
                         sites[tool.site_id] = tool
                     RoundRobinCounter[tool.site_id] = RoundRobinCounter[
-                        tool.site_id] + 1 
+                        tool.site_id] + 1
             else:
                 sites[tool.site_id] = min(sites[tool.site_id],
                                           tool,
