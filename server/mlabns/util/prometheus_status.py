4d60cfc37684c0e53c7c98e4165bf1d7fe64c645
Nathan Kinkade <nath@nkinka.de>
2018-03-29 15:48:53 -0600

    Adds a debugging logging statement.

--- server/mlabns/util/prometheus_status.py
+++ server/mlabns/util/prometheus_status.py
@@ -202,4 +202,6 @@ def get_slice_status(url, opener, slice_id):
         else:
             results[sliver_fqdn]['status'] = message.STATUS_OFFLINE
 
+        logging.info('STATUS: %s' % results[sliver_fqdn])
+
     return results
