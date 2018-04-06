1d09cff58f03fa33079534ff01ace60d856dc0aa
Nathan Kinkade <nath@nkinka.de>
2018-04-06 17:18:55 -0600

    Add numerous improvements to processing and output. Runs in an inchoate loop, which will tally metrics over time.

--- compare_instances.py
+++ compare_instances.py
@@ -4,7 +4,9 @@
 import argparse
 import json
 import logging
+import re
 import sys
+import time
 import urllib2
 
 QUERY_STRING = 'policy=all'
@@ -21,7 +23,7 @@ def parse_options(args):
         their values.
     """
     parser = argparse.ArgumentParser(
-        description='Compares the results from two different mlab-ns instances.')
+        description='Compares the results from two mlab-ns instances.')
     parser.add_argument(
         '--instance_one_url',
         dest='instance_one_url',
@@ -36,10 +38,21 @@ def parse_options(args):
                         dest='tool_id',
                         default='ndt',
                         help='The tool_id to query e.g., ndt.')
-    parser.add_argument('--all_fields',
-                        dest='all_fields',
-                        action="store_true",
-                        help='Whether to compare all fields or just "fqdn".')
+    parser.add_argument(
+        '--all_fields',
+        dest='all_fields',
+        action="store_true",
+        help='Compare all fields for equality, not just "fqdn".')
+    parser.add_argument(
+        '--infer_queueing',
+        dest='infer_queueing',
+        action="store_true",
+        help='Attempts to determine if queueing accounts for differences.')
+    parser.add_argument('--ignore_pattern',
+                        dest='ignore_patterns',
+                        nargs='*',
+                        default=[],
+                        help='Regex pattern to ignore, matched against "fqdn".')
 
     args = parser.parse_args(args)
 
@@ -71,9 +84,44 @@ def get_mlabns_status(url):
     return json_data
 
 
-def convert_sort_results(results, args):
+def filter_results(results, args):
+    """Filters results for any the user didn't want processed.
+    
+    Arguments:
+        results: list of dicts, with each dict containing status for a service.
+        args: argparse.ArgumentParser, options passed to script.
+    
+    Returns:
+        list: with any matching items filtered.
+    """
+    # Before processing the result list, check to see if the 'fqdn'
+    # field matches any --ignore_pattern options that may have been supplied by
+    # the user.
+    filtered_results = []
+    for key, status in enumerate(results):
+        matched = False
+        for pattern in args.ignore_patterns:
+            if re.search(pattern, status['fqdn']):
+                matched = True
+
+        if not matched:
+            filtered_results.append(results[key])
+
+    return filtered_results
+
+
+def convert_results(results, args):
+    """Converts JSON results into a python set().
+    
+    Arguments:
+        results: list of dicts, with each dict containing status for a service.
+        args: argparse.ArgumentParser, options passed to script.
+    
+    Returns:
+        set: representing the results passed to function.
+    """
     results_list = []
-    for result in sorted(results, key=lambda d: d['fqdn']):
+    for result in results:
         # TODO (kinkade): make this work for IPv6 too.
         # result['ip'] is a list with the first item being the IPv4 address and
         # the second the IPv6, if one exists.
@@ -85,40 +133,62 @@ def convert_sort_results(results, args):
         else:
             item = [result['fqdn']]
 
-        results_list.append(item)
+        results_list.append(tuple(item))
 
-    result_set = set(tuple(row) for row in results_list)
-    return result_set
+    return set(results_list)
 
 
 def compare_results(s1, s2):
-    s1_not_s2 = s1.difference(s2)
-    s2_not_s1 = s2.difference(s1)
+    """Compares two sets.
+    
+    Arguments:
+        s1: set, first set for comparison.
+        s2: set, first set for comparison.
+    
+    Returns:
+        dict, data with comparison metrics for sets.
+    """
+    data = {}
+    data['s1_count'] = len(s1)
+    data['s1_not_s2'] = len(s1.difference(s2))
+    data['s2_not_s1'] = len(s2.difference(s1))
+    data['s1_and_s2'] = len(s1.intersection(s2))
 
-    print 'Instance 1 and 2 agree on %d services.' % len(s1.intersection(s2))
-    print 'Instance 1 advertises these services which instance 2 does not:'
-    for item in sorted(s1_not_s2):
-        print '    %s' % (item,)
-
-    print 'Instance 2 advertises these services which instance 1 does not:'
-    for item in sorted(s2_not_s1):
-        print '    %s' % (item,)
+    return data
 
 
 def main():
     args = parse_options(sys.argv[1:])
 
-    url_one = '%s/%s?%s' % (args.instance_one_url, args.tool_id, QUERY_STRING)
-    results_one = get_mlabns_status(url_one)
-    logging.info('Results returned from instance one: %d', len(results_one))
-    set_one = convert_sort_results(results_one, args)
+    s1_not_s2_total = 0
+    s2_not_s1_total = 0
+    s1_and_s2_total = 0
 
-    url_two = '%s/%s?%s' % (args.instance_two_url, args.tool_id, QUERY_STRING)
-    results_two = get_mlabns_status(url_two)
-    logging.info('Results returned from instance two: %d', len(results_two))
-    set_two = convert_sort_results(results_two, args)
+    while True:
+        url_one = '%s/%s?%s' % (args.instance_one_url, args.tool_id,
+                                QUERY_STRING)
+        results_one = get_mlabns_status(url_one)
+        results_one_filtered = filter_results(results_one, args)
+        set_one = convert_results(results_one_filtered, args)
 
-    compare_results(set_one, set_two)
+        url_two = '%s/%s?%s' % (args.instance_two_url, args.tool_id,
+                                QUERY_STRING)
+        results_two = get_mlabns_status(url_two)
+        results_two_filtered = filter_results(results_two, args)
+        set_two = convert_results(results_two_filtered, args)
+
+        data = compare_results(set_one, set_two)
+        s1_and_s2_total = s1_and_s2_total + data['s1_and_s2']
+        s1_not_s2_total = s1_not_s2_total + data['s1_not_s2']
+        s2_not_s1_total = s2_not_s1_total + data['s2_not_s1']
+        print 'Instance 1 AND instance 2: %d' % data['s1_and_s2']
+        print 'Instance 1 NOT instance 2: %d' % data['s1_not_s2']
+        print 'Instance 2 NOT instance 1: %d' % data['s2_not_s1']
+        discrepancy = data['s1_not_s2'] / (data['s1_count'] * 1.0) * 100
+        print 'Discrepancy between 1 and 2: {0:.2f}%'.format(discrepancy)
+
+        # Sleep for 100s
+        time.sleep(100)
 
 
 if __name__ == '__main__':
