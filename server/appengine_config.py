3a7f35b4f4a266017d414779f994e457271cda1a
Nathan Kinkade <nath@nkinka.de>
2018-05-08 11:57:54 -0600

    Removes including third_party/google_cloudstorage, since that code is natice to GAE and was only necessary for local testing.

--- server/appengine_config.py
+++ server/appengine_config.py
@@ -6,4 +6,3 @@ def webapp_add_wsgi_middleware(app):
 # Add any libraries in the "mlabns/third_party" directory.
 from google.appengine.ext import vendor
 vendor.add('mlabns/third_party/geoip2')
-vendor.add('mlabns/third_party/google_cloudstorage')
