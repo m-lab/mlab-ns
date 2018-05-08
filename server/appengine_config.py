62fd02224cf1c712995fb877edc985c402ed1cc2
Nathan Kinkade <nath@nkinka.de>
2018-05-08 16:44:57 -0600

    Bumps instance class to F4_1G, and uses abspath for vendor.add() in appengine_config.py.

--- server/appengine_config.py
+++ server/appengine_config.py
@@ -1,3 +1,5 @@
+import os
+
 def webapp_add_wsgi_middleware(app):
     from google.appengine.ext.appstats import recording
     app = recording.appstats_wsgi_middleware(app)
@@ -5,5 +7,8 @@ def webapp_add_wsgi_middleware(app):
 
 # Add any libraries in the "mlabns/third_party" directory.
 from google.appengine.ext import vendor
-#vendor.add('mlabns/third_party/geoip2')
-#vendor.add('mlabns/third_party/google_cloudstorage')
+
+# Convoluted path as suggested from GAE documentation:
+# https://cloud.google.com/appengine/docs/standard/python/tools/using-libraries-python-27#requesting_a_library
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+    'mlabns/third_party/geoip2'))
