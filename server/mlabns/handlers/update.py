72631e959d5530ba9c58bbb19d45fe880b977087
Nathan Kinkade <nath@nkinka.de>
2018-06-20 11:45:20 -0600

    Don't append to tool_extra because it will grow out of control. Instead just update it. Fixes #151.

--- server/mlabns/handlers/update.py
+++ server/mlabns/handlers/update.py
@@ -439,9 +439,9 @@ class StatusUpdateHandler(webapp.RequestHandler):
                     slice_status[sliver_tool.fqdn] = {}
                     slice_status[sliver_tool.fqdn][
                         'status'] = message.STATUS_OFFLINE
-                    # Don't wipe out the IPv4 'tool_extra', just append to it.
+                    # Update tool_extra to signal that _ipv6 is not known.
                     slice_status[sliver_tool.fqdn][
-                        'tool_extra'] = sliver_tool.tool_extra + '(Family "_ipv6" for sliver not known by monitoring).'
+                        'tool_extra'] = constants.PROMETHEUS_TOOL_EXTRA + ' (Family "_ipv6" for sliver not known by monitoring).'
                 else:
                     # If monitoring data doesn't exist for this tool, append
                     # the sliver_tool unmodified to the list that gets written
