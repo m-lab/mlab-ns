9c5dd1ef883f5702d075ca3028e3dcd615d47b48
yachang <yachang@users.noreply.github.com>
2018-07-24 19:11:56 -0400

    Update lookup_query.py

--- server/mlabns/util/lookup_query.py
+++ server/mlabns/util/lookup_query.py
@@ -309,5 +309,7 @@ class LookupQuery:
             and policy. Otherwise, returns an empty string.
         """
         if self.ip_address and self.user_agent and self.tool_id and self.policy:
-            return self.ip_address + '#' + self.user_agent + '#' + self.tool_id + '#' + query.response_format + '#' + self.policy + '####'
+            return fmt.Sprintf("%s#%s#%s#%s#%s####",
+                               self.ip_address, self.user_agent, self.tool_id,
+                               self.response_format, self.Policy)
         return ''
