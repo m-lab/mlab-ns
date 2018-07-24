72ef43bba251b7adab476549999aa0e7f9df6704
yachang <yachang@users.noreply.github.com>
2018-07-24 19:17:14 -0400

    Update client_signature_fetcher.py

--- server/mlabns/db/client_signature_fetcher.py
+++ server/mlabns/db/client_signature_fetcher.py
@@ -18,8 +18,7 @@ class ClientSignatureFetcher(object):
             matched entry.
         """
         matched_requests = memcache.get(
-            key,
-            namespace=constants.MEMCACHE_NAMESPACE_REQUESTS)
+            key, namespace=constants.MEMCACHE_NAMESPACE_REQUESTS)
         if matched_requests and len(matched_requests) == 1:
             return matched_requests[0].probability
         return 0
