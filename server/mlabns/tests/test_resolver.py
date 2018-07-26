19a864a41c43bced2a7e254dfe2421cedf4d826e
yachang <changya@gmail.com>
2018-07-26 15:35:26 -0400

    half way

--- server/mlabns/tests/test_resolver.py
+++ server/mlabns/tests/test_resolver.py
@@ -818,6 +818,35 @@ class ResolverTestCase(unittest.TestCase):
             resolver.new_resolver('unrecognized_policy'),
             resolver.RandomResolver)
 
+class ClientSignatureFetcherTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_all_stubs()
+        ndb.get_context().clear_cache()
+
+        client_signature_fetcher_patch = mock.patch.object(
+            client_signature_fetcher,
+            'ClientSignatureFetcher',
+            autospec=True)
+        self.addCleanup(client_signature_fetcher_patch.stop)
+        client_signature_fetcher_patch.start()
+
+        self.fetcher = client_signature_fetcher.ClientSignatureFetcher()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def testFetchFromMemcache(self):
+        # The mock response is just ints here for simplicity, though the real
+        # function returns SliverTool objects.
+        mock_memcache_response = 0.1
+        client_signature_fetcher.ClientSignatureFetcher().fetch.return_value = (
+            mock_memcache_response)
+        fetcher_results_actual = self.fetcher.fetch('Faked_key')
+        self.assertEqual(mock_memcache_response, fetcher_results_actual)
+
 
 if __name__ == '__main__':
     unittest.main()
