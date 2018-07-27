7549fc7401f69e7ad6c8a08ab07af89f669f68e9
yachang <yachang@users.noreply.github.com>
2018-07-27 13:23:31 -0400

    Update test_client_signature_fetcher.py

--- server/mlabns/tests/test_client_signature_fetcher.py
+++ server/mlabns/tests/test_client_signature_fetcher.py
@@ -16,23 +16,13 @@ class ClientSignatureFetcherTestCase(unittest.TestCase):
         self.testbed.init_all_stubs()
         ndb.get_context().clear_cache()
 
-        client_signature_fetcher_patch = mock.patch.object(
-            client_signature_fetcher,
-            'ClientSignatureFetcher',
-            autospec=True)
-        self.addCleanup(client_signature_fetcher_patch.stop)
-        client_signature_fetcher_patch.start()
-
-        self.fetcher = client_signature_fetcher.ClientSignatureFetcher()
-
     def tearDown(self):
         self.testbed.deactivate()
 
     def testFetchFromMemcache(self):
-        # The mock response is just ints here for simplicity, though the real
-        # function returns SliverTool objects.
-        mock_memcache_response = 0.1
-        client_signature_fetcher.ClientSignatureFetcher().fetch.return_value = (
-            mock_memcache_response)
-        fetcher_results_actual = self.fetcher.fetch('Faked_key')
-        self.assertEqual(mock_memcache_response, fetcher_results_actual)
+        with mock.patch.object(client_signature_fetcher, 'ClientSignatureFetcher') as client_signature_fetcher_patch:
+            mock_memcache_response = 0.1
+            client_signature_fetcher.ClientSignatureFetcher().fetch.return_value = (
+                mock_memcache_response)
+            fetcher_results_actual = client_signature_fetcher.ClientSignatureFetcher().fetch('Faked_key')
+            self.assertEqual(mock_memcache_response, fetcher_results_actual)
