c7ff86ebb517e4283fb4fd2b0d38ba508b37085f
yachang <yachang@users.noreply.github.com>
2018-08-15 16:10:53 -0400

    Update update.py

--- server/mlabns/handlers/update.py
+++ server/mlabns/handlers/update.py
@@ -526,7 +526,7 @@ class BlacklistRequestsHandler(webapp.RequestHandler):
         requests = list(model.Requests.all().fetch(limit=None))
         for request in requests:
             if not memcache.set(request.key().name(),
-                                request.Probability,
+                                request.probability,
                                 namespace=constants.MEMCACHE_NAMESPACE_REQUESTS,
                                 time=900):
                 logging.error('Failed to update blacklist clients in memcache.')
