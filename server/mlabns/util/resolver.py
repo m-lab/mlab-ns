45c926dd688775cf34cb472bd64543b2d8b0b7c9
yachang <yachang@users.noreply.github.com>
2018-08-15 15:29:01 -0400

    Update resolver.py

--- server/mlabns/util/resolver.py
+++ server/mlabns/util/resolver.py
@@ -102,7 +102,7 @@ class GeoResolver(ResolverBase):
         prob = self.prob_of_blacklisted(query)
         logging.info('prob returned from memcache: %f', prob)
         tt = random.uniform(0, 1)
-        log.warning('rand: %f', tt)
+        logging.warning('rand: %f', tt)
         if prob < 1.0 and tt > prob:
             # Filter the candidates sites, only keep the '0c' sites
             filtered_candidates = filter(lambda c: c.site_id[-1] == 'c',
