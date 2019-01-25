296f12747b0b05d7bd8242e46d78cb16bca608eb
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-01-25 14:00:54 -0700

    Add sandbox deployment target, retire cron.yaml in favor of Cloud Scheduler (#168)
    * Support new sandbox environment & deployment to mlab-sandbox
    * Upate travis submodule
    * Replace legacy cron.yaml with cron.sh script
    * Add new environment specific app.yaml configs

---
+++ cron.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# cron.sh creates the cron jobs needed for the ns service, using the
+# Cloud Schedule API to create app engine cron jobs.
+
+set -ex
+PROJECT=${1:?Please provide project}
+BASEDIR="$(dirname "$0")"
+
+# Force correct gcloud into path
+export PATH=$HOME/google-cloud-sdk/bin:$PATH
+
+# check for site status, run every minute.
+"${BASEDIR}"/travis/schedule_appengine_job.sh "${PROJECT}" check_status \
+    --description="Check sliver tools status" \
+    --relative-url="/cron/check_status" \
+    --schedule="every 1 minutes" \
+    --http-method="GET" \
+    --service="locate"
+
+# check for new sites, and for new and/or updated IP addresses, roundrobin information.
+# run every 24 hours, starting at 06:00
+"${BASEDIR}"/travis/schedule_appengine_job.sh "${PROJECT}" check_site \
+    --description="Check sites, update IP and roundrobin" \
+    --relative-url="/cron/check_site" \
+    --schedule="every day 06:00" \
+    --http-method="GET" \
+    --service="locate"
+
+# Update blacklist of client signature list for abusive clients.
+"${BASEDIR}"/travis/schedule_appengine_job.sh "${PROJECT}" update_requests \
+    --description="Check client blacklists for abusive clients" \
+    --relative-url="/cron/update_requests" \
+    --schedule="every 5 minutes" \
+    --http-method="GET" \
+    --service="locate"
+
+# Report all currently scheduled jobs.
+gcloud --project "${PROJECT}" beta scheduler jobs list 2> /dev/null
