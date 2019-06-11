20882de80bed9f5267601330cf7cac859c696036
Stephen Soltesz <soltesz@google.com>
2019-06-11 17:39:38 -0400

    Add test coverage of no-content and not-found logic

--- server/mlabns/tests/test_lookup.py
+++ server/mlabns/tests/test_lookup.py
@@ -7,10 +7,24 @@ from mlabns.handlers import lookup
 from mlabns.util import constants
 from mlabns.util import reverse_proxy
 
+from google.appengine.ext import testbed
+
 
 class LookupTest(unittest2.TestCase):
 
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
     class OutMockup:
+
+        def __init__(self):
+            self.msg = ''
 
         def write(self, msg):
             self.msg = msg
@@ -29,14 +43,19 @@ class LookupTest(unittest2.TestCase):
 
     class RequestMockup:
 
-        def __init__(self, url='', path=''):
+        def __init__(self, url='', path='', qs=''):
             self.response = mock.Mock()
             self.error_code = None
             self.url = url
             self.scheme = url[:url.index(':')]
             self.path = path
-            self.path_qs = path
+            self.path_qs = path + ('?' + qs if qs else '')
             self.uri = url
+            self.remote_addr = '127.0.0.1'
+            self.headers = {'X-AppEngine-CityLatLong': '30,120',}
+
+        def get(self, key, default_value=''):
+            return getattr(self, key, default_value)
 
         def error(self, error_code):
             self.error_code = error_code
@@ -69,7 +88,7 @@ class LookupTest(unittest2.TestCase):
             'https://new-mlab-ns.appspot.com')
         h = lookup.LookupHandler()
         h.request = LookupTest.RequestMockup(url='https://mlab-ns.appspot.com',
-                                             path='/ndt_ssl')
+                                             path='/ndt_ssl?policy=geo_options')
         h.response = LookupTest.ResponseMockup()
         mock_urlopen.return_value = LookupTest.URLLibResponseMockup(
             'any-fake-data', {'content-type': 'application/json'})
@@ -79,6 +98,49 @@ class LookupTest(unittest2.TestCase):
         # Check response headers, and fake content.
         self.assertEqual(h.response.out.msg, 'any-fake-data')
         self.assertEqual(h.response.headers['Content-Type'], 'application/json')
+
+    @mock.patch.object(reverse_proxy, 'try_reverse_proxy_url')
+    def test_get_with_no_content(self, mock_try_reverse_proxy_url):
+        tool_a = model.Tool(tool_id='tool_a')
+        tool_b = model.Tool(tool_id='tool_b')
+        tool_a.put()
+        tool_b.put()
+        # Skip reverse proxy.
+        mock_try_reverse_proxy_url.return_value = ''
+
+        sliver_a = model.SliverTool(
+            tool_id='tool_a',
+            status='online',
+            site_id='foo01',
+            slice_id='tool_a',
+            fqdn='a.tool.mlab1.foo01.measurement-lab.org',
+            sliver_ipv4='192.168.1.2')
+        sliver_a.put()
+
+        h = lookup.LookupHandler()
+        h.request = LookupTest.RequestMockup(url='https://mlab-ns.appspot.com',
+                                             path='/tool_a',
+                                             qs='policy=geo_options')
+        h.response = LookupTest.ResponseMockup()
+
+        h.get()
+
+        self.assertEqual(h.response.out.msg, '')
+
+    @mock.patch.object(reverse_proxy, 'try_reverse_proxy_url')
+    def test_get_with_not_found(self, mock_try_reverse_proxy_url):
+        # Skip reverse proxy.
+        mock_try_reverse_proxy_url.return_value = ''
+
+        h = lookup.LookupHandler()
+        h.request = LookupTest.RequestMockup(url='https://mlab-ns.appspot.com',
+                                             path='/FAKE_TOOL_ID',
+                                             qs='policy=geo_options')
+        h.response = LookupTest.ResponseMockup()
+
+        h.get()
+
+        self.assertEqual(h.response.out.msg, '{"status_code": "404 Not found"}')
 
     def test_log_location(self):
         h = lookup.LookupHandler()
