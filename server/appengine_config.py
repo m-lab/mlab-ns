cc2acb65c878c3696df049abfb709f3d70a69afc
Nathan Kinkade <kinkade@measurementlab.net>
2021-10-11 16:37:06 -0600

    Disables AppEngine "AppStats" collection
    As far as I know we have never used the data collected by AppStats, and
    AppStats adds some overhead to every request:
    
    https://cloud.google.com/appengine/docs/standard/python/tools/appstats
    
    We can reenable this later, if we find some need for it.

--- server/appengine_config.py
+++ server/appengine_config.py
@@ -1,10 +1,5 @@
 import os
 
-def webapp_add_wsgi_middleware(app):
-    from google.appengine.ext.appstats import recording
-    app = recording.appstats_wsgi_middleware(app)
-    return app
-
 # Add any libraries in the "mlabns/third_party" directory.
 from google.appengine.ext import vendor
 
