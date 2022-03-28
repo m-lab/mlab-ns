5b539c62afffb06be145d9115b8503c2f3cf3934
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-03-28 15:45:19 -0400

    Increase cloud sites to 10 pct (#258)

--- server/mlabns/util/resolver.py
+++ server/mlabns/util/resolver.py
@@ -1,10 +1,8 @@
 import logging
 import random
 
-from mlabns.db import sliver_tool_fetcher
-from mlabns.util import distance
-from mlabns.util import message
-from mlabns.db import client_signature_fetcher
+from mlabns.db import client_signature_fetcher, sliver_tool_fetcher
+from mlabns.util import distance, message
 
 
 def _tool_properties_from_query(query):
@@ -58,20 +56,20 @@ class AllResolver(ResolverBase):
 # selecting this site. The default value is 1.0.
 site_keep_probability = {
     'bom01': 0.5,
-    'bru06': 0.01,  # virtual site
-    'cgk01': 0.01,  # virtual site
-    'fra07': 0.01,  # virtual site
+    'bru06': 0.1,  # virtual site
+    'cgk01': 0.1,  # virtual site
+    'fra07': 0.1,  # virtual site
     'hnd01': 0.05,  # 0.05
-    'iad07': 0.01,  # virtual site
-    'lax07': 0.01,  # virtual site
+    'iad07': 0.1,  # virtual site
+    'lax07': 0.1,  # virtual site
     'lga1t': 0.5,
-    'lhr09': 0.01,  # virtual site
+    'lhr09': 0.1,  # virtual site
     'lis01': 0.5,
     'lju01': 0.5,
-    'ord07': 0.01,  # virtual site
-    'sea09': 0.01,  # virtual site
-    'sin02': 0.01,  # virtual site
-    'tpe02': 0.01,  # virtual site
+    'ord07': 0.1,  # virtual site
+    'sea09': 0.1,  # virtual site
+    'sin02': 0.1,  # virtual site
+    'tpe02': 0.1,  # virtual site
     'tun01': 0.5,
     'vie01': 0.5,
     'yqm01': 0.5,
